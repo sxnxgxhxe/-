90fd3ef6c2bea571877f6c78f175712da285ba8c
sxnxgxhxe <163002175+sxnxgxhxe@users.noreply.github.com>
2024-03-26 11:48:17 +0900

    C++

---
+++ tjejrr.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b;
+	char ch;
+
+	printf("첫 번째 수를 입력하세요 :");
+	scanf_s("%d", &a);
+
+	printf("계산할 연산자를 입력하세요 :");
+	scanf_s(" %c", &ch);
+
+	printf("두 번째 수를 입력하세요 :");
+	scanf_s("%d", &b);
+
+	if (ch == '+')
+		printf("%d + %d = %d 입니다. \n", a, b, a + b);
+
+	else if (ch == '-')
+		printf("%d - %d = %d 입니다. \n", a, b, a - b);
+
+	else if (ch == '*')
+		printf("%d * %d = %d 입니다. \n", a, b, a * b);
+
+	else if (ch == '/')
+		printf("%d / %d = %f 입니다. \n", a, b, a / (float)b);
+
+	else if (ch == '%')
+		printf("%d %% %d = %d 입니다. \n", a, b, a % b);
+
+	else
+		printf("연산자를 잘못 입력했습니다");
+}
